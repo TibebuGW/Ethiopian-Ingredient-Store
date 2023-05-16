bf9c28e6e63e3d0d54beb88357e9205976a1623f
Tibebu <tibebugwas@gmail.com>
2023-05-16 14:53:50 +0300

    start login

--- Frontend/src/components/Sign Up/Signup.jsx
+++ Frontend/src/components/Sign Up/Signup.jsx
@@ -1,8 +1,65 @@
-import {Link} from 'react-router-dom'
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+const REGISTER_URL = "auth/signup";
+
 export const Signup = () => {
+  const [firstName, setFirstName] = useState("");
+  const [secondName, setSecondName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleFirstNameChange = (e) => {
+    setFirstName(e.target.value);
+  };
+
+  const handleSecondNameChange = (e) => {
+    setSecondName(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleConfirmPasswordChange = (e) => {
+    setConfirmPassword(e.target.value);
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+
+    try {
+      // eslint-disable-next-line no-unused-vars
+      const response = await axios.post(
+        `${import.meta.env.VITE_REACT_APP_BASE_URL}${REGISTER_URL}`,
+        JSON.stringify({ email, password }),
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        }
+      );
+
+      // eslint-disable-next-line no-constant-condition
+      if (200){
+        this.props.history.push('/login')
+      }
+    } catch (error) {
+      console.log("auth error: ", error);
+    }
+  };
+
   return (
     <div className="w-full h-screen flex items-center justify-center">
-      <form className="py-3 w-30p px-10 overflow-y-auto rounded-2xl shadow-md box-border bg-[#d6dce9]">
+      <form
+        className="py-3 w-30p px-10 overflow-y-auto rounded-2xl shadow-md box-border bg-[#d6dce9]"
+        onSubmit={handleClick}
+      >
         <div className="grid grid-cols-2 gap-2 my-2">
           <div className="my-2">
             <label className="block my-2">First Name</label>
@@ -10,6 +67,8 @@ export const Signup = () => {
               className="box-border shadow py-2 px-5 rounded-md w-100p placeholder:font-medium"
               placeholder="First name"
               type="text"
+              value={firstName}
+              onChange={handleFirstNameChange}
             />
           </div>
           <div className="my-2">
@@ -18,6 +77,8 @@ export const Signup = () => {
               className="box-border shadow py-2 px-5 rounded-md w-100p placeholder:font-medium"
               placeholder="Second name"
               type="text"
+              value={secondName}
+              onChange={handleSecondNameChange}
             />
           </div>
         </div>
@@ -27,6 +88,8 @@ export const Signup = () => {
             className="box-border shadow py-2 px-5 rounded-md w-100p placeholder:font-medium"
             placeholder="Email"
             type="email"
+            value={email}
+            onChange={handleEmailChange}
           />
         </div>
         <div className="my-2">
@@ -35,6 +98,8 @@ export const Signup = () => {
             className="box-border shadow py-2 px-5 rounded-md w-100p placeholder:font-medium"
             placeholder="Password"
             type="password"
+            value={password}
+            onChange={handlePasswordChange}
           />
         </div>
         <div className="my-2">
@@ -43,13 +108,19 @@ export const Signup = () => {
             className="box-border shadow py-2 px-5 rounded-md w-100p placeholder:font-medium"
             placeholder="Confirm password"
             type="password"
+            value={confirmPassword}
+            onChange={handleConfirmPasswordChange}
           />
         </div>
         <button className="py-2 px-5 my-3 text-secondary rounded-md w-100p bg-lightPrimary hover:bg-darkPrimary">
           Sign up
         </button>
         <p className="my-3 text-center">
-          Already have an account? <Link to="/login"> <span className="text-blue">Login</span></Link>
+          Already have an account?{" "}
+          <Link to="/login">
+            {" "}
+            <span className="text-blue">Login</span>
+          </Link>
         </p>
       </form>
     </div>
