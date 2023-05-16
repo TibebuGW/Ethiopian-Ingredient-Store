80eb587f39de202ebcf64756e556b4d0e6339734
Tibebu <tibebugwas@gmail.com>
2023-05-16 20:42:53 +0300

    backend integration for all items

--- Frontend/src/components/Create Item/CreateItem.jsx
+++ Frontend/src/components/Create Item/CreateItem.jsx
@@ -1,71 +1,107 @@
-import { BsCamera } from "react-icons/bs";
-import profile from "../../assets/images/profile.jpg";
+import { useState } from "react";
+import axios from "axios";
 import Navbar from "../Navbar/Navbar";
+import { useNavigate } from "react-router-dom";
+
+const CREATE_ITEM_URL = "item";
+
 const CreateItem = () => {
+  const [name, setname] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handlenameChange = (e) => {
+    setname(e.target.value);
+  };
+  const handleDescriptionChange = (e) => {
+    setDescription(e.target.value);
+  };
+  const handlePriceChange = (e) => {
+    setPrice(e.target.value);
+  };
+  const handleImageChange = (e) => {
+    setImage(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const data = { name, description, price, image }
+    axios({
+        method: "post",
+        url: `${import.meta.env.VITE_REACT_APP_BASE_URL}${CREATE_ITEM_URL}`,
+        data: data,
+        headers: { "Content-Type": "multipart/form-data" },
+      })
+      .then((response) => {
+        console.log(response.data)
+        navigate("/")
+      })
+      .catch(function (error) {
+        console.log(error);
+   });
+    
+  };
+
   return (
     <div>
       <Navbar />
       <div className="flex items-center justify-center">
-        <p className="text-4xl mt-10 font-bold">CreateItem</p>
+        <p className="text-4xl mt-10 font-bold">Create Item</p>
       </div>
-      <div className="flex ml-10 mt-10">
-        <div className="flex flex-col justify-start  w-25p">
-          <img
-            src={profile}
-            className="h-[200px] w-[200px] rounded-lg shadow-lg mx-auto mt-5 mb-5"
-            alt="Avatar"
-          />
-          <BsCamera style={{ fontSize: "35px", margin: "0 auto", cursor: "pointer"}} />
-        </div>
-        <div className="">
-          <form className="py-3 w-100p px-10 overflow-y-auto rounded-2xl shadow-xl box-border">
-            <div className="grid grid-cols-2 gap-2 my-2">
-              <div className="my-2">
-                <label className="block my-2">First Name</label>
-                <input
-                  className="box-border shadow py-2 px-5 rounded-md w-100p placeholder:font-medium"
-                  placeholder="First name"
-                  type="text"
-                />
-              </div>
-              <div className="my-2">
-                <label className="block my-2">Second Name</label>
-                <input
-                  className="box-border shadow py-2 px-5 rounded-md w-100p placeholder:font-medium"
-                  placeholder="Second name"
-                  type="text"
-                />
-              </div>
-            </div>
+      <div className="flex justify-center">
+        <form className="py-3 w-50p px-10 mt-10 overflow-y-auto rounded-2xl shadow-xl box-border">
+          <div className="grid grid-cols-2 gap-2 my-2">
             <div className="my-2">
-              <label className="block my-2">Email</label>
+              <label className="block my-2">name</label>
               <input
                 className="box-border shadow py-2 px-5 rounded-md w-100p placeholder:font-medium"
-                placeholder="Email"
-                type="email"
+                placeholder="name"
+                type="text"
+                value={name}
+                onChange={handlenameChange}
               />
             </div>
             <div className="my-2">
-              <label className="block my-2">Password</label>
+              <label className="block my-2">Price</label>
               <input
                 className="box-border shadow py-2 px-5 rounded-md w-100p placeholder:font-medium"
-                placeholder="Password"
-                type="password"
+                placeholder="Price"
+                type="text"
+                value={price}
+                onChange={handlePriceChange}
               />
             </div>
-            <div className="my-2">
-              <label className="block my-2">Confirm Password</label>
-              <input
-                className="box-border shadow py-2 px-5 rounded-md w-100p placeholder:font-medium"
-                placeholder="Confirm password"
-                type="password"
-              />
-            </div>
-            <button className="py-2 px-5 my-3 text-secondary rounded-md w-100p bg-lightPrimary hover:bg-darkPrimary">
-              Create Item
-            </button>
-          </form>
-        </div>
+          </div>
+          <div className="my-2">
+            <label className="block my-2">Description</label>
+            <input
+              className="box-border shadow py-2 px-5 rounded-md w-100p placeholder:font-medium"
+              placeholder="Description"
+              type="text"
+              value={description}
+              onChange={handleDescriptionChange}
+            />
+          </div>
+          <div className="my-2">
+            <label className="block my-2">Image</label>
+            <input
+              className="box-border shadow py-2 px-5 rounded-md w-100p placeholder:font-medium"
+              type="file"
+              value={image}
+              onChange={handleImageChange}
+            />
+          </div>
+          <button
+            className="py-2 px-5 my-3 text-secondary rounded-md w-100p bg-lightPrimary hover:bg-darkPrimary"
+            onClick={handleSubmit}
+          >
+            Create Item
+          </button>
+        </form>
       </div>
     </div>
   );
--- Frontend/src/components/Home/Items.jsx
+++ Frontend/src/components/Home/Items.jsx
@@ -1,6 +1,24 @@
+import { useEffect, useState } from 'react'
 import ProductItem from "../Cards/ProductItem";
-import { items } from "../../api/Items";
+import axios from 'axios'
+
+const GET_ALL_ITEMS_URL = "item/all"
+
 const Items = () => {
+  const [items, setItems] = useState([])
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${import.meta.env.VITE_REACT_APP_BASE_URL}${GET_ALL_ITEMS_URL}`);
+        setItems(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
 
   return (
     <div className="grid grid-cols-4 gap-4 mt-10">
